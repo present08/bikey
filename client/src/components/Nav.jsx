5a4451b6f16df9e93d0976902b9ae9ea32eab54c
present08 <present_08@naver.com>
2025-06-08 17:04:44 +0900

    서버 통신경로 수정

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = ({ token, setToken }) => {
+    const [checkToken, setCheckToken] = useState()
+
+    useEffect(() => {
+        setCheckToken(localStorage.getItem("token"))
+        console.log(token)
+    }, [])
+
     const Logout = () => {
         localStorage.clear();
         setToken(null);
+        setCheckToken(null)
     }
 
     return (
         <nav style={{ padding: '1rem', background: '#eee' }}>
             <Link to="/shipping">출고</Link>
             <Link to="/register">데이터 편집</Link>
-            {token == null ? <Link to="/login">login</Link> : <button onClick={Logout}>Logout</button>}
+            {checkToken == null ? <Link to="/login">login</Link> : <button onClick={Logout}>Logout</button>}
         </nav>
     )
 }
