67090414584517dbd130c32ddfbb7b8e62818abe
present08 <present_08@naver.com>
2025-06-08 13:45:36 +0900

    ubuntuTest

--- server/src/main/java/com/bikey/server/model/BikeyProduct.java
+++ server/src/main/java/com/bikey/server/model/BikeyProduct.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Product {
+public class BikeyProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- server/src/main/java/com/bikey/server/repository/ProductRepository.java
+++ server/src/main/java/com/bikey/server/repository/ProductRepository.java
@@ -6,18 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.bikey.server.model.Product;
+import com.bikey.server.model.BikeyProduct;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT productName FROM Product where division = :param")
+public interface ProductRepository extends JpaRepository<BikeyProduct, Long> {
+    @Query("SELECT productName FROM BikeyProduct where division = :param")
     List<String> findByProductName(@Param("param") String param);
 
-    @Query("SELECT transName FROM Product where division = :param")
+    @Query("SELECT transName FROM BikeyProduct where division = :param")
     List<String> findByProductTransName(@Param("param") String param);
 
-    @Query("SELECT productName FROM Product where division IN :param")
+    @Query("SELECT productName FROM BikeyProduct where division IN :param")
     List<String> findByListProductName(@Param("param") List<String> param);
 
-    @Query("SELECT transName FROM Product where division IN :param")
+    @Query("SELECT transName FROM BikeyProduct where division IN :param")
     List<String> findByListProductTranName(@Param("param") List<String> param);
 }
